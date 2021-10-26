ac5c568bc7b11f02dbc6141374d856560f2be253
ABHI <abhishekitte11@gmail.com>
2021-10-26 17:08:32 +0530

    [ABHISHEK] Added a code to create address book table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -1,3 +1,19 @@
 ----------UC1--Create Addressbook Database------------ 
 create database AddressBookService;
 use AddressBookService;
+
+-------------UC2-------------
+------Create table------------------
+
+create table AddressBookTable
+(
+FirstName varchar(100) not null,
+LastName varchar(50),
+Address varchar(200),
+City varchar(150),
+StateName varchar(100),
+ZipCode int,
+Phonenum bigint,
+EmailId varchar(100)
+);
+select * from AddressBookTable;
