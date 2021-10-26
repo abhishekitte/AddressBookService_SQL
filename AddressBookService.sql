127524aae7e4471d86c8a664157cbd0e0b0f0f30
ABHI <abhishekitte11@gmail.com>
2021-10-26 17:27:24 +0530

    [ABHISHEK] Added a code to update existing contact address book table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -26,3 +26,12 @@ Insert into AddressBookTable values('Abhishek','Itte','Worli','Mumbai','Maharast
 ('Ganesh','Mudigonda','Dadar','Mumbai','Maharastra',400025,993053254,'ganeshm2@gmail.com'),
 ('Naveen','chil','Worli','Mumbai','Maharahtra',40014,8080277459,'naveenchi23@gmail.com');
 select * from AddressBookTable;
+
+
+--------------------UC4----------------------------------------
+------edit existing contact person using person name----------
+update AddressBookTable set Phonenum=9944789324 where FirstName='Anvesh';
+update AddressBookTable set LastName='Chil' where FirstName='Naveen';
+update AddressBookTable set EmailId='virat18@gmail.com' where LastName='Mudigonda';
+update AddressBookTable set ZipCode='400034' where FirstName='Abhishek';
+select * from AddressBookTable;
